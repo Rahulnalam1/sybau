5fdb73e4372ead852af6b0fc72f8075656b42d4c
rahulnalam1 <rahulnalam1@gmail.com>
2025-03-29 14:24:01 -0500

    pushing sybau icon

--- sybau/src/components/team-switcher.tsx
+++ sybau/src/components/team-switcher.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import * as React from "react"
-import { ChevronsUpDown, Plus } from "lucide-react"
+import { ChevronsUpDown, Plus, Flower2 } from "lucide-react"
 
 import {
   DropdownMenu,
@@ -81,7 +81,7 @@ export function TeamSwitcher({
             <DropdownMenuSeparator />
             <DropdownMenuItem className="gap-2 p-2">
               <div className="flex size-6 items-center justify-center rounded-md border bg-background">
-                <Plus className="size-4" />
+                <Flower2 className="size-4" />
               </div>
               <div className="font-medium text-muted-foreground">Add team</div>
             </DropdownMenuItem>
