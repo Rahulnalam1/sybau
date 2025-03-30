74ad18e878dea49dc6ff112ba9c22c05cc0db4a7
Aryan-Bahl <abahl3@illinois.edu>
2025-03-30 09:04:38 -0500

    updated auth page

--- sybau/src/app/authentication/page.tsx
+++ sybau/src/app/authentication/page.tsx
@@ -6,11 +6,8 @@ export default function LoginPage() {
   return (
     <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-muted p-6 md:p-10">
       <div className="flex w-full max-w-sm flex-col gap-6">
-        <a href="#" className="flex items-center gap-2 self-center font-medium">
-          <div className="flex h-6 w-6 items-center justify-center rounded-md bg-primary text-primary-foreground">
-            <Flower2 className="size-4" />
-          </div>
-          Sybau
+        <a href="#" className="flex items-center gap-3 self-center font-semibold text-2xl md:text-5xl">
+          sybau 🥀
         </a>
         <LoginForm />
       </div>
