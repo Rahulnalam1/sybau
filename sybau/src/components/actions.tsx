cbc902b2661da7f1fa933498f52fd2ad85e65093
rahulnalam1 <rahulnalam1@gmail.com>
2025-03-30 09:02:34 -0500

    pushing location of action

--- sybau/src/components/actions.tsx
+++ sybau/src/components/actions.tsx
@@ -394,7 +394,8 @@ export function EmailCommandButton({ editor }: { editor?: Editor | null }) {
 
   return (
     <>
-      <div className="fixed bottom-8 left-1/2 transform -translate-x-1 z-10" ref={buttonRef}>
+      <div className="fixed bottom-8 left-1/2 transform -translate-x-1/2clear
+       z-10" ref={buttonRef}>
         {!open ? (
           <Button
             className="ml-auto bg-white hover:bg-gray-50 text-gray-900 rounded-[12px] border border-gray-200 shadow-sm flex items-center gap-4 px-5 py-3 text-sm h-11"
